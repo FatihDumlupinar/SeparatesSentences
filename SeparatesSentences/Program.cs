7749fe0b8b46baa9ec850b54654a394ed6671ce0
Fatih DUMLUPINAR <fatih_dmlpnr@outlook.com>
2023-03-31 11:50:12 +0300

    Add project files.

---
+++ SeparatesSentences/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+string input = "In the beginning, the earth was without form. And void. And then the sun shone upon the sleeping earth. Into this swirling maelstrom of fire air and water, the first stirrings of life appeared.";
+
+string[] sentences = Regex.Split(input, @"(?<=[\.!\?])\s+");
+
+foreach (string sentence in sentences)
+{
+    Console.WriteLine(sentence);
+}
+
+Console.ReadLine();
